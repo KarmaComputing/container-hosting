e9d7cc66ff48bea4f886ce10d0eaaa4c015b2ecb
chrisjsimpson <chris.j.simpson@live.co.uk>
2022-08-04 19:59:28 +0100

    display dokku comands the exact same as they appear in official docs aid learning, but still use allow_list
    This is for developer experience so that a dev can still rever to the official dokku documentation to learn the commands meaning

--- dokku-wrapper.py
+++ dokku-wrapper.py
@@ -4,8 +4,11 @@ import subprocess
 import sqlite3
 import json
 
-api_key_to_container = {}
+print("In dokku-wrapper.py")
 
+#https://github.com/pyca/pynacl/issues/192
+
+api_key_to_container = {}
 # Example mapping between secret api keys to container app names
 """
 api_key_to_container = {
@@ -34,7 +37,6 @@ for row in cur.execute('SELECT * FROM container'):
     api_key_to_container[[*container.keys()][0]] = container[[*container.keys()][0]]
 
 
-print("In dokku-wrapper.py")
 
 unsafe_api_key = sys.argv[1]
 print(f"Checking unsafe_api_key: {unsafe_api_key}")
@@ -47,40 +49,60 @@ except KeyError as e:
     exit()
 
 print("Valid api key")
-APP_NAME = api_key_to_container[unsafe_api_key]['APP_NAME']
-print(f"app_name: {APP_NAME}")
 
-unsafe_requested_command = sys.argv[2]
+try:
+    APP_NAME = api_key_to_container[unsafe_api_key]['APP_NAME']
+    GITHUB_USERNAME = api_key_to_container[unsafe_api_key]["GITHUB_USERNAME"]
+    print(f"app_name: {APP_NAME}")
+except KeyError as e:
+    print(f"Error getting app setting: {e}")
+    exit()
 
-commands_allowlist = {
-    "app-create": "dokku apps:create APP_NAME",
-    "set-docker-build-dir": "dokku builder:set APP_NAME build-dir src",
-    "set-docker-Dockerfile-path": "dokku builder-dockerfile:set APP_NAME dockerfile-path Dockerfile",
-    "git-sync": "dokku git:sync --build APP_NAME https://github.com/GITHUB_USERNAME/APP_NAME.git main",
-    "certs-add": "dokku certs:add APP_NAME < cert-key.tar",
-    "set-env-RAILS_DEVELOPMENT_HOSTS": "dokku config:set --no-restart APP_NAME RAILS_DEVELOPMENT_HOSTS=APP_URL",
-    "set-env-DATABASE_URL": "dokku config:set --no-restart APP_NAME DATABASE_URL=RAILS_DATABASE_URL",
-    "set-env-SECRET_KEY": "dokku config:set --no-restart APP_NAME SECRET_KEY=DJANGO_SECRET_KEY",
-    "set-env-ALLOWED_HOSTS": "dokku config:set --no-restart APP_NAME ALLOWED_HOSTS='APP_URL'",
-    "set-env-DEBUG": "dokku config:set --no-restart APP_NAME DEBUG=DJANGO_DEBUG",
-    "set-env-DJANGO_ENGINE":"dokku config:set --no-restart APP_NAME DB_ENGINE=DJANGO_ENGINE",
-    "set-env-DJANGO_DB_NAME": "dokku config:set --no-restart APP_NAME DB_NAME=DJANGO_DB_NAME",
-    "set-env-DJANGO_DB_HOST": "dokku config:set --no-restart APP_NAME DB_HOST=DJANGO_DB_HOST",
-    "set-env-DJANGO_DB_USER": "dokku config:set --no-restart APP_NAME DB_USER=DJANGO_DB_USER",
-    "set-env-DJANGO_DB_PASSWORD": "dokku config:set --no-restart APP_NAME DB_PASSWORD=DJANGO_DB_PASSWORD",
-    "set-env-DJANGO_DB_PORT": "dokku config:set --no-restart APP_NAME DB_PORT=DJANGO_DB_PORT",
-}
+unsafe_requested_command = ''
+MAX_ARGS=7
+if len(sys.argv) > MAX_ARGS:
+    print(f"Too many args. Got {len(sys.argv)} expected {MAX_ARGS}")
+    exit()
 
-if unsafe_requested_command not in commands_allowlist:
+for arg in sys.argv[2:MAX_ARGS]:
+    unsafe_requested_command += f"{arg} "
+unsafe_requested_command = unsafe_requested_command.strip()
+
+commands_allowlist = [
+    "dokku apps:create APP_NAME",
+    "dokku builder:set APP_NAME build-dir src",
+    "dokku builder-dockerfile:set APP_NAME dockerfile-path Dockerfile",
+    "dokku git:sync --build APP_NAME https://github.com/GITHUB_USERNAME/APP_NAME.git main",
+    "dokku certs:add APP_NAME < cert-key.tar",
+    "dokku config:set --no-restart APP_NAME RAILS_DEVELOPMENT_HOSTS=APP_URL",
+    "dokku config:set --no-restart APP_NAME DATABASE_URL=RAILS_DATABASE_URL",
+    "dokku config:set --no-restart APP_NAME SECRET_KEY=DJANGO_SECRET_KEY",
+    "dokku config:set --no-restart APP_NAME ALLOWED_HOSTS='APP_URL'",
+    "dokku config:set --no-restart APP_NAME DEBUG=DJANGO_DEBUG",
+    "dokku config:set --no-restart APP_NAME DB_ENGINE=DJANGO_ENGINE",
+    "dokku config:set --no-restart APP_NAME DB_NAME=DJANGO_DB_NAME",
+    "dokku config:set --no-restart APP_NAME DB_HOST=DJANGO_DB_HOST",
+    "dokku config:set --no-restart APP_NAME DB_USER=DJANGO_DB_USER",
+    "dokku config:set --no-restart APP_NAME DB_PASSWORD=DJANGO_DB_PASSWORD",
+    "dokku config:set --no-restart APP_NAME DB_PORT=DJANGO_DB_PORT",
+    ]
+
+valid_command = False
+
+for allowed_command in commands_allowlist:
+    allowed_command = allowed_command.replace("APP_NAME", APP_NAME).replace("GITHUB_USERNAME", GITHUB_USERNAME)
+    print(f"Comparing: {allowed_command} with {unsafe_requested_command}")
+    if allowed_command ==  unsafe_requested_command:
+        valid_command = True
+        break;
+
+if valid_command is False:
     print("Invalid command. Possible commands are:")
-    for command_example in commands_allowlist.keys():
-        print(command_example)
+    for possible_command in commands_allowlist:
+        print(possible_command.replace("APP_NAME", APP_NAME).replace("GITHUB_USERNAME", GITHUB_USERNAME))
     exit()
 else:
     print("Valid command")
-    safer_command = commands_allowlist[unsafe_requested_command].replace(
-        "APP_NAME", APP_NAME
-    )
-    print(f"Safer command: {safer_command}")
+    print(f"Allowed command: {allowed_command}")
     print("Running command")
-    subprocess.run(safer_command, shell=True)
+    subprocess.run(allowed_command, shell=True)
