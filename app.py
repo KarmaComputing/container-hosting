d58f1acf5403ca7eebf5eaa40771bf4aa0fe9360
chrisjsimpson <chris.j.simpson@live.co.uk>
2023-04-22 15:40:17 +0100

    pass git_org to deploy_yml

--- app.py
+++ app.py
@@ -643,7 +643,7 @@ async def githubcallback(request):
         f"{BASE_PATH}tmp-cloned-repos/{APP_NAME}/.github/workflows/deploy.yml", "w"
     ) as fp:
         deploy_yml = deploy_yml.replace("$APP_NAME", APP_NAME)
-        deploy_yml = deploy_yml.replace("$GIT_USERNAME_OR_ORG", username)
+        deploy_yml = deploy_yml.replace("$GIT_USERNAME_OR_ORG", git_org)
         deploy_yml = deploy_yml.replace("$GIT_REPO_NAME", repo_name)
         fp.write(deploy_yml)
 
